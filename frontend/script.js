ca802dd8bb7aa2368d76af3c086318f53009910a
Maximillian Eldrich Zhu <eldrichzhu@gmail.com>
2026-02-07 16:52:30 +0700

    Made button do stuff

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,11 @@
-console.log("Frontend JS loaded!");
+const saveBtn = document.getElementById("saveBtn");
+
+saveBtn.addEventListener("click", () => {
+    const book = document.getElementById("bookTitle").value;
+    const chapter = document.getElementById("bookChapter").value;
+    const content = document.getElementById("bookContent").value;
+
+    console.log("Book: ", book);
+    console.log("Chapter: ", chapter);
+    console.log("Content: ", content);
+});
