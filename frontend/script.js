bfcbb5c9741050f046f485d10cfff2ef3416f885
Maximillian Eldrich Zhu <eldrichzhu@gmail.com>
2026-02-15 13:57:48 +0700

    Cleaned up formatting, added editable content instead of input

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ const saveBtn = document.getElementById("saveBtn");
 saveBtn.addEventListener("click", () => {
     const book = document.getElementById("bookTitle").value;
     const chapter = document.getElementById("chapterTitle").value;
-    const content = document.getElementById("chapterContent").value;
+    const content = document.getElementById("chapterContent").innerText.trim();
 
     books.push({
         book,
