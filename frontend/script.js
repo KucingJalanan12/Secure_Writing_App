4a53c96b0a1581531588d86224e4eb788f9ba0d0
Maximillian Eldrich Zhu <eldrichzhu@gmail.com>
2026-04-04 17:10:57 +0700

    Added working placeholder text

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ const saveBtn = document.getElementById("saveBtn");
 
 saveBtn.addEventListener("click", () => {
     const book = document.getElementById("bookTitle").value;
-    const chapter = document.getElementById("chapterTitle").value;
+    const chapter = document.getElementById("chapterTitle").innerText.trim();
     const content = document.getElementById("chapterContent").innerText.trim();
 
     books.push({
@@ -14,3 +14,27 @@ saveBtn.addEventListener("click", () => {
 
     console.log("Books array: ", books);
 });
+
+const editor = document.getElementById("chapterContent");
+const editor2 = document.getElementById("chapterTitle");
+
+function handleEmpty(element) {
+    if (element.innerText.trim() === "") {
+        element.classList.add("empty");
+    } else {
+        element.classList.remove("empty");
+    }
+}
+function handleEnter(element, e) {
+    if (e.key === "Backspace" || e.key === "Delete") {
+        if(element.innerText.trim() === "" || element.innerText.trim() === "\n"){
+            e.preventDefault();
+            element.innerHTML = "";
+        }
+    }
+}
+
+editor.addEventListener("input", () => handleEmpty(editor));
+editor.addEventListener("keydown", (e) => handleEnter(editor, e));
+editor2.addEventListener("input", () => handleEmpty(editor2));
+editor2.addEventListener("keydown", (e) => handleEnter(editor2, e));
